9a1439b19a8d084d3b58dba96da7b5da775e25cf
Yonatan Klein <yonatanklein058@gmail.com>
2022-10-24 20:36:15 +0300

    First Commit

---
+++ Stage0/Program1764.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome1764();
+        Welcome1863();
+        Console.ReadKey();
+    }
+
+    private static void Welcome1764()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+
+    static partial void Welcome1863();
+}
