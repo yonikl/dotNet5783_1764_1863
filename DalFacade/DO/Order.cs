78a821110b0f384c93e510c884bf82d1f5a1494d
raz <raz.leibovitch1@gmail.com>
2022-11-03 21:22:12 +0200

    Pushing the intialize

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,4 +1,4 @@
-﻿namespace DO;
+namespace DO;
 
 public struct Order
 {
--- DalList/DataSource .cs
+++ DalList/DataSource .cs
@@ -1,11 +1,50 @@
-﻿using DO;
-namespace Dal;
+﻿namespace Dal;
 using DO;
 
 internal static class DataSource
 {
-    static readonly int Random;
-    internal static Order[] Orders = new Order[200];
+    static readonly Random Generator = new Random();
+    internal static Order[] Orders = new Order[100];
     internal static OrderItem[] OrdersItems = new OrderItem[200];
     internal static Product[] Products = new Product[50];
+    
+    private static int OrdersSize = 0;
+    private static int ProductsSize = 0;
+    private static int OrdersItemSize = 0;
+
+    public static void AddNewOrder(Order order)
+    {
+        if (OrdersSize < 100)
+        {
+            Orders[OrdersSize] = order;
+            OrdersSize++;
+        }
+    }
+
+    public static void AddNewOrderItem(OrderItem orderItem)
+    {
+        if (OrdersItemSize < 200)
+        {
+            OrdersItems[OrdersSize] = orderItem;
+            OrdersItemSize++;
+        }
+    }
+
+    public static void AddNewProduct(Product product)
+    {
+        if (ProductsSize < 50)
+        {
+            Products[ProductsSize] = product;
+            ProductsSize++;
+        }
+    }
+
+    static DataSource()
+    {
+        s_Initialize();
+    }
+    private static void s_Initialize()
+    {
+
+    }
 }
