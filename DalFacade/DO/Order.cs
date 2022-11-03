0f59efafacdce6c2e45ce8eb980f7d4f59d94667
raz <raz.leibovitch1@gmail.com>
2022-11-03 20:03:47 +0200

    before pulling

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Xml.Linq;
+﻿
 
 namespace DO;
 
--- DalList/DataSource .cs
+++ DalList/DataSource .cs
@@ -1,4 +1,5 @@
-﻿namespace Dal;
+﻿using DO;
+namespace Dal;
 
 internal static class DataSource
 {
@@ -6,4 +7,18 @@ internal static class DataSource
     internal static DalOrder[] Orders = new DalOrder[200];
     internal static DalOrderItem[] OrdersItems = new DalOrderItem[200];
     internal static DalProduct[] Products = new DalProduct[50];
+
+    private static void AddNewOrder(DalOrder Order)
+    {
+        Orders.Append(Order);
+    }
+    private static void AddNewOrderItem(DalOrderItem  orderItem)
+    {
+        OrdersItems.Append(orderItem);
+    }
+    private static void AddNewProduct(DalProduct Product)
+    {
+        Products.Append(Product);
+    }
+
 }
