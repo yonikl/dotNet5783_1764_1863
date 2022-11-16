7547c5ab70a895cc31f2922bc353ed17434b9fed
Yonatan Klein <yonatanklein058@gmail.com>
2022-11-16 16:15:08 +0200

    update BlInterfaces

--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -1,8 +1,12 @@
-﻿
+﻿using BO;
 namespace BlApi;
 
 public interface IOrder
 {
-
+    public IEnumerable<OrderForList> GetAllOrders();
+    public Order GetOrder(int id);
+    public Order UpdateShipping(int id);
+    public Order UpdateDelivery(int id);
+    public OrderTracking TrackOrder(int id);
 }
 
--- BL/BlApi/IProduct.cs
+++ BL/BlApi/IProduct.cs
@@ -3,6 +3,11 @@ namespace BlApi;
 
 public interface IProduct
 {
-
+    public IEnumerable<BO.ProductForList> GetAllItems();
+    public BO.Product GetProductForAdmin(int id);
+    public BO.ProductItem GetProductForUser(int id);
+    public void AddProduct(BO.Product item);
+    public void UpdateProduct(BO.Product item);
+    public void RemoveProduct(BO.Product item);
 }
 
