e5e63126acf00c4c91b64215cc4f8cc8e04c803e
raz <raz.leibovitch1@gmail.com>
2022-11-29 18:34:46 +0200

    pushing new windows.

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -3,7 +3,7 @@ using DalApi;
 /// <summary>
 /// Class to manage the dal
 /// </summary>
-public class DalList : IDal
+public class DalList :  IDal
 {
     public IProduct Product => new DalProduct();
     public IOrderItem OrderItem => new DalOrderItem();
